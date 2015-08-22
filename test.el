50e2de506a54dd45743bb13f541f1218b1aa2166
supermomonga <hi@supermomonga.com>
2015-08-22 16:20:33 +0900

    get file dir

--- test.el
+++ test.el
@@ -5,9 +5,14 @@
   (setq server-port "1234")
   (setq server-use-tcp t)
   (setq server-name "emacs_server")
-  (setq server-auth-dir "/Users/momonga/Develops/dockerfile-emacs-daemon/serverd/")
-  )
+  (let ((file-dir
+         (mapconcat 'identity
+                    (append
+                     (reverse (cdr (reverse (split-string (buffer-file-name) "/"))))
+                     '("serverd"))
+                    "/")))
+    (setq server-auth-dir file-dir)))
 
-(server-eval-at "emacs_server" "(+ 1 10)")
+(server-eval-at "emacs_server" '(+ 1 10))
 
 (server-running-p)
